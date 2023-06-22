48c1ea4fc485270178377ae6f37ee0a85b48b22e
Pedro Miguel <pedro.miguel.e.idk@gmail.com>
2023-06-22 15:44:00 -0300

    Work on error notification.
    Added styling on error banner.
    Split skeleton into different folder.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import windows_logo from "./assets/microsoft-windows-22-logo-svgrepo-com.svg";
 import React from "react";
+import Skeleton from "./components/skeleton_cards";
 
 export type game = {
   id: number;
@@ -15,12 +16,6 @@ export type game = {
   freetogame_profile_url: string;
 };
 
-enum Erros {
-  "NetworkError" = "NetworkError",
-  "TimeoutError" = "TimeoutError",
-  "AbortError" = "AbortError",
-  "UnknownError" = "UnknownError",
-}
 
 export default function App() {
   const [error, setError] = React.useState<string | null>(null);
@@ -80,8 +75,8 @@ export default function App() {
   }, []);
 
   return (
-    <div className="overflow-y-auto flex flex-col items-center w-full px-5">
-      <h1 className="text-4xl font-bold text-gray-800 my-10 w-full max-w-7xl ">
+    <div className="min-h-screen overflow-y-auto flex flex-col items-center w-full px-5">
+      <h1 className="text-4xl font-black text-gray-800 my-10 w-full max-w-7xl ">
         Games
       </h1>
       <GameList games={games} err={error} />
@@ -101,14 +96,12 @@ export function GameList({ games, err }: GameListProps) {
     const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const year = date.getFullYear().toString();
 
-    return `${day}-${month}-${year}`;
+    return `${day}/${month}/${year}`;
   };
+
   if (err) {
-    return <p>{err}...</p>;
+    return <div className=" text-center text-2xl font-semibold m-10 p-10 bg-white rounded-lg shadow-md flex justify-center">{err}...</div>;
   }
-
-  //return <Skeleton />;
-
   return (
     <ul
       style={{ gridTemplateColumns: " repeat(auto-fill, minmax(270px, 1fr))" }}
@@ -120,14 +113,14 @@ export function GameList({ games, err }: GameListProps) {
         games.map((game) => (
           <li
             key={game.id}
-            className="hover:scale-105 transition-all duration-300"
+            className=" hover:scale-95 md:hover:scale-105 transition-all duration-300"
           >
             <div className="flex flex-col bg-white shadow-md rounded-lg p-4 w-full h-full max">
               <img
                 src={game.thumbnail}
                 alt={game.title}
                 loading="lazy"
-                className="object-cover object-center rounded-lg mb-4"
+                className="object-cover object-center rounded-md mb-4 aspect-video"
               />
               <div
                 className="
@@ -135,12 +128,12 @@ export function GameList({ games, err }: GameListProps) {
                   w-full h-full 
                 "
               >
-                <h2 className="text-2xl font-bold text-gray-900">
+                <h2 className="text-2xl font-black text-gray-900">
                   {game.title}
                 </h2>
-                <p className="text">
-                  {game.short_description} released on{" "}
-                  {formatDate(game.release_date)}{" "}
+                <p className="text ">
+                  {game.short_description} Released on{" "}
+                  {formatDate(game.release_date)}.
                 </p>
               </div>
               <p className="font-light uppercase text-sm px-1">{game.genre}</p>
@@ -161,9 +154,11 @@ export function GameList({ games, err }: GameListProps) {
               <a
                 href={game.freetogame_profile_url}
                 className="
-                    bg-blue-400 px-6 py-3 rounded-lg text-center text-white font-bold
+                    bg-blue-500 px-6 py-3 rounded-lg text-center text-white font-bold
                     shadow-md
-                    hover:bg-blue-600 transition-colors duration-300
+                    hover:bg-blue-600
+                    hover:border-blue-600
+                    transition-colors duration-300
                   "
               >
                 More info
@@ -174,78 +169,4 @@ export function GameList({ games, err }: GameListProps) {
       )}
     </ul>
   );
-}
-
-export function Skeleton() {
-  const qnt: number[] = Array(12).fill(0);
-
-  return qnt.map((_, index) => (
-    <li key={index} className="hover:scale-105 transition-all duration-300">
-      <div className="flex flex-col bg-white shadow-md rounded-lg p-4 w-full h-full max">
-        <div role="status" className="animate-pulse w-full">
-          <div className="flex items-center justify-center w-full h-48 bg-gray-300 rounded dark:bg-gray-700">
-            <svg
-              className="w-12 h-12 text-gray-200"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 640 512"
-            >
-              <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
-            </svg>
-          </div>
-          <span className="sr-only">Loading...</span>
-        </div>
-        <div
-          className="
-              flex flex-col align-center mb-3
-              w-full h-full 
-            "
-        >
-          <div role="status" className="max-w-sm animate-pulse">
-            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 my-4 py-2"></div>
-            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
-            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
-            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
-            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
-            <span className="sr-only">Loading...</span>
-          </div>
-        </div>
-        <div className="w-24 flex px-1">
-          <div className="w-full h-2.5 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-        </div>
-        <div className="flex flex-row gap-10 justify-between items-center py-3 pt-0 px-1">
-          <div className="w-80 h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 "></div>
-          <div role="status">
-            <svg
-              aria-hidden="true"
-              className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-              viewBox="0 0 100 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                fill="currentColor"
-              />
-              <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill="currentFill"
-              />
-            </svg>
-            <span className="sr-only">Loading...</span>
-          </div>
-        </div>
-        <a
-          className="
-              bg-gray-200 rounded-full dark:bg-gray-700 px-6 py-3 rounded-md text-center text-white font-bold
-                shadow-md
-              "
-        >
-          Carregando
-        </a>
-      </div>
-    </li>
-  ));
 }
---
+++ src/components/skeleton_cards.tsx
@@ -0,0 +1,73 @@
+export default function Skeleton() {
+    const qnt: number[] = Array(12).fill(0);
+  
+    return qnt.map((_, index) => (
+      <li key={index} className="hover:scale-105 transition-all duration-300">
+        <div className="flex flex-col bg-white shadow-md rounded-lg p-4 w-full h-full max">
+          <div role="status" className="animate-pulse w-full">
+            <div className="flex items-center justify-center w-full h-48 bg-gray-300 rounded dark:bg-gray-700">
+              <svg
+                className="w-12 h-12 text-gray-200"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 640 512"
+              >
+                <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
+              </svg>
+            </div>
+            <span className="sr-only">Loading...</span>
+          </div>
+          <div
+            className="
+                flex flex-col align-center mb-3
+                w-full h-full 
+              "
+          >
+            <div role="status" className="max-w-sm animate-pulse">
+              <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 my-4 py-2"></div>
+              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+              <span className="sr-only">Loading...</span>
+            </div>
+          </div>
+          <div className="w-24 flex px-1">
+            <div className="w-full h-2.5 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+          </div>
+          <div className="flex flex-row gap-10 justify-between items-center py-3 pt-0 px-1">
+            <div className="w-80 h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 "></div>
+            <div role="status">
+              <svg
+                aria-hidden="true"
+                className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span className="sr-only">Loading...</span>
+            </div>
+          </div>
+          <a
+            className="
+                bg-gray-200 rounded-full dark:bg-gray-700 px-6 py-3 rounded-md text-center text-white font-bold
+                  shadow-md
+                "
+          >
+            Carregando
+          </a>
+        </div>
+      </li>
+    ));
+  }
